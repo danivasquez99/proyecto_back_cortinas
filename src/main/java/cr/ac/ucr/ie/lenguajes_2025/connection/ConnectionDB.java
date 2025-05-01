90e01e143a4590c836eb43ba882154c7ed6b021f
AntonyML <antony.mongelopez@ucr.ac.cr>
2025-05-01 12:33:38 -0600

    Agrega .gitignore y archivos iniciales del proyecto Spring Boot

---
+++ src/main/java/cr/ac/ucr/ie/lenguajes_2025/connection/ConnectionDB.java
@@ -0,0 +1,44 @@
+package cr.ac.ucr.ie.lenguajes_2025.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Daniel
+ */
+public class ConnectionDB {
+    
+    private final static String DATABASE = "lenguajes-2025";
+    
+    private final static String USER = "root";
+    
+    private final static String PASSWORD = "12345";
+    
+    private final static String HOST = "localhost";
+    
+    private final static int PORT = 3306;
+    
+    private final static String URL =
+            "jdbc:mysql://"+HOST+":"+PORT+"/"+DATABASE;
+    
+    private static Connection connect;
+    
+    public static Connection getConnection(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error al importar la clase" + e.getMessage());
+        }
+        
+        try {
+            connect = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Se estableció la conexión");
+        } catch (SQLException e) {
+            System.out.println("Error al establecer la conexión: " + e.getMessage());
+        }
+        
+        return connect;
+    }
+}
---
+++ src/main/java/cr/ac/ucr/ie/lenguajes_2025/dao_implement/UserDAOImplement.java
@@ -0,0 +1,168 @@
+package cr.ac.ucr.ie.lenguajes_2025.dao_implement;
+
+import cr.ac.ucr.ie.lenguajes_2025.connection.ConnectionDB;
+import cr.ac.ucr.ie.lenguajes_2025.dao.UserDAO;
+import cr.ac.ucr.ie.lenguajes_2025.domain.User;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author Daniel
+ */
+public class UserDAOImplement implements UserDAO {
+
+    @Override
+    public LinkedList<User> getAll() {
+        LinkedList<User> usersList = new LinkedList<User>();
+
+        StringBuilder sql = new StringBuilder();
+        sql.append("SELECT idUser, name, birthdate, email, password, ");
+        sql.append("urlProfilePicture, role, isActive, creationDate ");
+        sql.append("FROM user WHERE isActive = 1");
+
+        try {
+            Connection cn = ConnectionDB.getConnection();
+            PreparedStatement ps = cn.prepareStatement(sql.toString());
+            ResultSet rs = ps.executeQuery();
+
+            User user;
+
+            while (rs.next()) {
+                user = new User();
+                user.setIdUser(rs.getInt(1));
+                user.setName(rs.getString(2));
+                user.setBirthdate(LocalDate.parse(rs.getString(3)));
+                user.setEmail(rs.getString(4));
+                user.setPassword(rs.getString(5));
+                user.setUrlProfilePicture(rs.getString(6));
+                user.setRole(rs.getString(7));
+                user.setIsActive(rs.getString(8) == "1" ? true : false);
+                user.setCreationDate(LocalDate.parse(rs.getString(9)));
+
+                usersList.add(user);
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Error al cargar todos los usuarios: " + e.getMessage());
+        }
+
+        return usersList;
+    }
+
+    @Override
+    public void insert(User t) {
+        StringBuilder sql = new StringBuilder();
+        sql.append("INSERT INTO user");
+        sql.append("(idUser, name, birthdate, email, password, ");
+        sql.append("urlProfilePicture, role, isActive, creationDate) ");
+        sql.append("VALUES (?,?,?,?,?,?,?,?,?)");
+
+        try {
+            Connection cn = ConnectionDB.getConnection();
+            PreparedStatement ps = cn.prepareStatement(sql.toString());
+            ps.setInt(1, t.getIdUser());
+            ps.setString(2, t.getName());
+            ps.setDate(3, Date.valueOf(t.getBirthdate()));
+            ps.setString(4, t.getEmail());
+            ps.setString(5, t.getPassword());
+            ps.setString(6, t.getUrlProfilePicture());
+            ps.setString(7, t.getRole());
+            ps.setString(8, t.isIsActive() ? "1" : "0");
+            ps.setDate(9, Date.valueOf(t.getCreationDate()));
+
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println("Error al insertar usuario: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void update(User t) {
+        StringBuilder sql = new StringBuilder();
+        sql.append("UPDATE user SET ");
+        sql.append("idUser=?, name=?, birthdate=?, email=?, password=?, ");
+        sql.append("urlProfilePicture=?, role=?, isActive=? ");
+        sql.append("WHERE idUser=?");
+
+        try {
+            Connection cn = ConnectionDB.getConnection();
+            PreparedStatement ps = cn.prepareStatement(sql.toString());
+            ps.setInt(1, t.getIdUser());
+            ps.setString(2, t.getName());
+            ps.setDate(3, Date.valueOf(t.getBirthdate()));
+            ps.setString(4, t.getEmail());
+            ps.setString(5, t.getPassword());
+            ps.setString(6, t.getUrlProfilePicture());
+            ps.setString(7, t.getRole());
+            ps.setString(8, t.isIsActive() ? "1" : "0");
+
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println("Error al actualizar usuario: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void deleteById(Integer t) {
+        StringBuilder sql = new StringBuilder();
+        sql.append("UPDATE user SET isActive=0 ");
+        sql.append("WHERE idUser=?");
+
+        try {
+            Connection cn = ConnectionDB.getConnection();
+            PreparedStatement ps = cn.prepareStatement(sql.toString());
+            ps.setInt(1, t);
+
+            ps.executeUpdate();
+        } catch (Exception e) {
+            System.err.println("Error al eliminar usuario: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public User findById(Integer t) {
+        User user = new User();
+
+        StringBuilder sql = new StringBuilder();
+        sql.append("SELECT user idUser, name, birthdate, email, password, ");
+        sql.append("urlProfileImage, role, isActive, creationDate");
+        sql.append("WHERE idUser=?");
+
+        try {
+
+            Connection cn = ConnectionDB.getConnection();
+            PreparedStatement ps = cn.prepareStatement(sql.toString());
+            ps.setInt(1, t);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                user.setIdUser(rs.getInt(1));
+                user.setName(rs.getString(2));
+                user.setBirthdate(LocalDate.parse(rs.getDate(3).toString()));
+                user.setEmail(rs.getString(4));
+                user.setPassword(rs.getString(5));
+                user.setUrlProfilePicture(rs.getString(6));
+                user.setRole(rs.getString(7));
+                user.setIsActive(rs.getString(8).equals("1") ? true : false);
+                user.setCreationDate(LocalDate.parse(rs.getDate(9).toString()));
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Error al cargar usuario: " + e.getMessage());
+        }
+
+        return user;
+    }
+
+    @Override
+    public boolean validateExistingEmail(String email) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
